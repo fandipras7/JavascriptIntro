bfefb9903d374be6a4aabe0ec9a8acad6cb15b5a
fandipras7 <fandiprasetyopro@gmail.com>
2022-04-19 07:54:40 +0700

    menambah validasi pada soal3js

--- soal3.js
+++ soal3.js
@@ -1,43 +1,43 @@
-function cariUrutanAngka(nilaiAwal, nilaiAkhir, array) {
-
-    const allInput = [nilaiAwal, nilaiAkhir, ...array];
+function seleksiNilai(nilaiAwal, nilaiAkhir, array) {
     let sortAngka;
-    if (allInput.every(a => typeof(a) === 'number')) {
-        if (nilaiAwal < nilaiAkhir && array.length > 5) {
-            sortAngka = array
-            .sort((a, b) => a - b)
-            .filter(a => a > nilaiAwal && a < nilaiAkhir);
-            if (sortAngka.length === 0) { console.log('Nilai tidak ditemukan'); }
+    let allInput;
+    if (Array.isArray(array)) {
+        allInput = [nilaiAwal, nilaiAkhir, ...array];
 
-            console.log(sortAngka);
+        if (allInput.every(a => typeof(a) === 'number')) {
+            if (nilaiAwal < nilaiAkhir && array.length > 5) {
+                sortAngka = array
+                    .sort((a, b) => a - b)
+                    .filter(a => a > nilaiAwal && a < nilaiAkhir);
+
+                sortAngka.length > 0 ? console.log(sortAngka) : console.log('Nilai Tidak ditemukan');;
+
+            } else {
+                nilaiAwal >= nilaiAkhir ? console.log('Nilai akhir harus lebih besar dari nilai awal') :
+                    console.log('Jumlah angka dalam dataArray harus lebih dari 5');
+            }
         } else {
-            nilaiAwal > nilaiAkhir ? console.log('Nilai Awal harus lebih besar dari Nilai Akhir') :
-                console.log('Panjang array harus lebih dari 5');
+            console.log('input yang anda masukan bukan angka');
         }
+
     } else {
-        console.log('input yang anda masukan bukan angka');
+        console.log('Harap masukan array');
     }
+
+
 
 }
 
-cariUrutanAngka(40, 35, [2, 25, 3, 30, 8, 10]);
+seleksiNilai(5, 20, [2, 25, 4, 14, 17, 30, 8]);
+
+seleksiNilai(4, 3, [2, 3, 3, 30, 8, 10]);
+
+seleksiNilai(4, 5, [2, 3, 3, 30, 8]);
+
+seleksiNilai(5, 17, [2, 25, 4, 1, 30, 18])
 
 
-
-// function cariUrutanAngka(nilaiAwal, nilaiAkhir, array) {
-//     let cekNumber = [nilaiAwal, nilaiAkhir, ...array];
-//     for (let i = 0; i < cekNumber.length; i++) {
-//         if (typeof(cekNumber[i]) !== 'number') {
-//             return 'harap masukan angka'
-//         }
-//     }
-//     let resultOfsort;
-//     if (nilaiAwal < nilaiAkhir && array.length > 5) {
-//         resultOfsort = array.filter(a => a > nilaiAwal && a < nilaiAkhir);
-//         if (resultOfsort.length === 0) { return 'Nilai tidak ditemukan' }
-//     } else {
-//         return nilaiAwal > nilaiAkhir ? 'Nilai akhir harus lebih besar dari nilai awal' : 'jumlah data harus lebih dari 5';
-//     }
-
-//     return resultOfsort;
-// }
+// Tambahan Validasi
+seleksiNilai('1', 12, [2, 25, 4, 1, 30, 18])
+seleksiNilai(1, 12, { nama: 'fandi' })
+seleksiNilai('1', 12, 'fandi')
